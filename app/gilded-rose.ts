0df5e189d7ab7d26dba1a8b83bb4118b1bc0c902
nikuz <micurino@gmail.com>
2021-01-23 12:29:37 -0800

    improve quality change logic

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -47,34 +47,35 @@ export class GildedRose {
                 continue;
             }
 
+            const sellIn = item.sellIn;
+            const quality = item.quality;
+
+            item.sellIn--;
+
+            if (quality === 0 || quality === 50) {
+                continue;
+            }
+
             if (!isAgedBrie(item) && !isBackstagePasses(item)) {
-                if (item.quality > 0) {
-                    const qualityDegradation = isConjured(item) ? 2 : 1;
-                    item.quality = item.quality - qualityDegradation
-                }
-            } else if (item.quality < 50) {
+                const qualityDegradation = isConjured(item) ? 2 : 1;
+                item.quality = item.quality - qualityDegradation
+            } else {
                 let qualityAdvance = 1;
                 if (isBackstagePasses(item)) {
-                    qualityAdvance = item.sellIn <= 5 ? 3 : 2;
+                    qualityAdvance = sellIn <= 5 ? 3 : 2;
                 }
                 item.quality += qualityAdvance;
             }
 
-            item.sellIn = item.sellIn - 1;
-
             if (item.sellIn < 0) {
                 if (!isAgedBrie(item)) {
                     if (!isBackstagePasses(item)) {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1
-                        }
+                        item.quality = item.quality - 1
                     } else {
                         item.quality = item.quality - item.quality
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
-                    }
+                    item.quality = item.quality + 1
                 }
             }
         }
